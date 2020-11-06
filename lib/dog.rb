be65e851ff6024ff1cd740749cd91d67687b23ff
Desiree Lerma <desi.lerma96@yahoo.com>
2020-11-05 19:37:15 -0600

    commit tree 8232bdd922634b699f043ac6dfaac888c6115329

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
-  # code goes here
+
+  attr_reader :name # reads the value cannot change
+  # writer can change but doesn't read
+  attr_accessor :mood, :owner 
+
+  @@all = []
+
+  def initialize(name, owner, mood = "nervous")
+    @owner = owner
+    @name = name
+    @mood = mood
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,74 @@
 class Owner
-  # code goes here
+
+
+  attr_accessor :cat, :dog, :pets
+  attr_reader :species, :name
+
+  @@all = []
+  @@pets = {:dogs => [], :cats => []}
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end
+
+  def say_species
+    "I am a #{species}."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.count
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def pets
+    @@pets
+  end
+
+  def cats
+    Cat.all.select {|cat| cat.owner == self}
+  end
+
+  def dogs
+    Dog.all.select {|dog| dog.owner == self}
+  end
+
+  def buy_cat(name)
+    #make new inst. of app. pet, inti, it w/ that name and owner who is purchasing
+    Cat.new(name, self)
+  end
+
+  def buy_dog(name)
+    #make new inst. of app. pet, inti, it w/ that name and owner who is purchasing
+    Dog.new(name, self)
+  end
+
+  def walk_dogs
+    self.dogs.each {|dog| dog.mood = "happy"}
+  end
+
+  def feed_cats
+    self.cats.each {|cat| cat.mood = "happy"}
+  end
+
+  def sell_pets
+    pets = self.dogs + self.cats
+    pets.each do |pet| 
+      pet.mood = "nervous"
+      pet.owner = nil
+    end
+  end
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
+
 end
